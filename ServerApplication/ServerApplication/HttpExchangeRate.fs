424be8cc7838a845e6d064fb9f5aaebc96e1894d
Alanmugz <Alanmugz@gmail.com>
2015-02-12 16:19:49 +0000

    Working with ex rates

---
+++ ServerApplication/ServerApplication/HttpExchangeRate.fs
@@ -0,0 +1,17 @@
+﻿
+
+namespace InvoiceApp
+
+module HttpRates = 
+    
+open System
+open HttpClient
+open FSharp.Data
+
+type Simple = JsonProvider<""" {"to": "EUR", "rate": 0.88111099999999998, "from": "USD"} """>
+
+let getRates convertFrom convertTo : decimal =
+    let url = "http://rate-exchange.appspot.com/currency?from=" + convertFrom + "&to=" +  convertTo
+    let page = (createRequest Get url |> getResponseBody)
+    let parsedRates = Simple.Parse(page)
+    parsedRates.Rate
